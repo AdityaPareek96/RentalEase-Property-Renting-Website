9c82a777c3bca256000dd1902ff717d302155be0
Aditya Pareek <adityapareek9649@gmail.com>
2025-06-04 22:36:40 +0530

    Update init/index.js with new latest changes

--- init/index.js
+++ init/index.js
@@ -1,23 +1,59 @@
+// require("dotenv").config(); //  LOAD ENV VARIABLES
+
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
+// // const MongoURL = "mongodb://127.0.0.1:27017/wanderlust";
+// const dbURL = process.env.ATLASDB_URL;
+
+// main().then(() => {
+//     console.log("Connected to DB.");
+// }).catch((err) => {console.log(err); });
+
+// async function main() {
+//     await mongoose.connect(dbURL);
+// };
+
+// const initDB = async () => {
+//     await Listing.deleteMany({});
+//     initData.data = initData.data.map((obj) => ({...obj, owner: "682dacf165a50fb372539450"}))
+//     await Listing.insertMany(initData.data);
+//     console.log("data initialized.");
+// };
+
+// initDB();
+
+require('dotenv').config({ path: '../.env' }); // LOAD ENV VARIABLES
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const Listing = require("../models/listing.js")
+const Listing = require("../models/listing.js");
 
-// const MongoURL = "mongodb://127.0.0.1:27017/wanderlust";
+console.log(process.env.ATLASDB_URL)
 const dbURL = process.env.ATLASDB_URL;
 
-main().then(() => {
-    console.log("Connected to DB.");
-}).catch((err) => {console.log(err); });
-
 async function main() {
+  try {
     await mongoose.connect(dbURL);
-};
+    console.log("Connected to DB.");
+
+    await initDB();
+
+    mongoose.connection.close();
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "682dacf165a50fb372539450"}))
-    await Listing.insertMany(initData.data);
-    console.log("data initialized.");
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "682dacf165a50fb372539450",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data initialized.");
 };
 
-initDB();
+main();
